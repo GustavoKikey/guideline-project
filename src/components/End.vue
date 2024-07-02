00ddebe61b68ec2c70623ad50d4e8e2cb5b293a6
Gustavo Kikey <gustavokakinohana@jera.com.br>
2024-07-02 17:10:15 -0400

    [UPDATE] new end forms

--- src/components/End.vue
+++ src/components/End.vue
@@ -7,12 +7,23 @@ import { RouterLink, RouterView } from "vue-router";
     <v-container>
       <h1>Obrigado por participar</h1>
       <p>
-        Clique no botão abaixo para acessar o formulário de feedback e nos conte
-        sua experiência com o conteúdo.
+        Clique em um dos botões abaixo para acessar o formulário de feedback e
+        nos conte sua experiência com o conteúdo.
       </p>
-      <v-btn href="https://forms.gle/wzH8Cycbeed2LjAS7"
-        >Acessar formulário</v-btn
-      >
+      <v-row rows="12">
+        <v-col cols="6">
+          <v-btn
+            href="https://docs.google.com/forms/d/e/1FAIpQLSeLFNeYPtAsJM2fMXRjI_Bu2BD4g-STLfGXm3zXK6p4xL7O7Q/viewform"
+            >Formulário para Estudantes</v-btn
+          >
+        </v-col>
+        <v-col cols="6">
+          <v-btn
+            href="https://docs.google.com/forms/d/e/1FAIpQLSf32hfikFdNNZnUsWpmsAHsFXC1IkZbRaY1a8iSKIC9nkGcLg/viewform"
+            >Formulário para Pedagogos</v-btn
+          >
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
